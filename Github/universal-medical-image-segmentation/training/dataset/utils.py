398a4ed7879b214c4d6ec127a489dfbce3f17860
quan91200 <xb91200@gmail.com>
2025-07-19 16:10:05 +0700

    Create hermes_resunet_3d

--- Github/universal-medical-image-segmentation/training/dataset/utils.py
+++ Github/universal-medical-image-segmentation/training/dataset/utils.py
@@ -3,7 +3,7 @@
 def get_dataset(args, mode, **kwargs):
     
     if args.dimension == '3d':
-        if args.dataset == 'universal':
+        if args.dataset in ['universal', 'kits']:
             from .dim3.dataset_universal import UniversalDataset
 
             return UniversalDataset(args, dataset_list=kwargs['dataset_name_list'], mode=mode)
